9405ac28252f6bd80b55f2c914aa8d2fdfb25ed5
mur4ik18 <o.o.kotov1999@gmail.com>
2024-04-23 15:29:53 +0200

    Add interface for pcs

--- test/src/app/pcs.ts
+++ test/src/app/pcs.ts
@@ -1,2 +1,32 @@
 export interface Pcs {
+    name: string;
+    marque: string;
+    type: string;
+    ecran: string;
+    clavier: string;
+    system: {
+        ram: number;
+        cpu: {
+            marque: string;
+            nom: string;
+            score: number;
+            core: number;
+            frequenceMax: number;
+            tdp: number;
+        };
+        hdd: {
+            capacite: number;
+            type: string;
+            rpm: number;
+            vitesseTransfert: number;
+        };
+        cg: {
+            marque: string;
+            modele: string;
+            score: number;
+        };
+    };
+    batterie: string;
+    prix: number;
+
 }
