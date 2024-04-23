e89b7bf43f98e3f4b8bca82c94ff00b500fffa0c
mur4ik18 <o.o.kotov1999@gmail.com>
2024-04-23 13:54:33 +0200

    je suis stupid

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, ContactComponent],
+  imports: [RouterOutlet, FooterComponent, ContactComponent, HomeComponent, HeaderComponent],
   //template: "<h1>Hello World!</h1>",
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Pest';
 }
