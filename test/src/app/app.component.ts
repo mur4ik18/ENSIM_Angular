0ee4077b88c993349dd2730ea671392159700502
Renaud44 <Segnon.Agbanglanon.Etu@univ-lemans.fr>
2024-04-23 13:27:10 +0100

    add changes before reset

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: "<h1>Hello World!</h1>",
-  //templateUrl: './app.component.html',
+  imports: [RouterOutlet, FooterComponent, ContactComponent],
+  //template: "<h1>Hello World!</h1>",
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
