1ee27111a0e31b3dc8412292a54bec9f0a6e22af
mur4ik18 <o.o.kotov1999@gmail.com>
2024-05-07 15:29:55 +0200

    Add choice pc page

--- test/src/app/routes.ts
+++ test/src/app/routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { ChoicePCComponent } from './choice-pc/choice-pc.component';
 
 const routeConfig: Routes = [
     {
       path: '',
       component: HomeComponent,
       title: 'Home page'
+    },
+    {
+        path: 'buypc',
+        component: ChoicePCComponent,
+        title: 'BuyPC page'
     },
     {
       path: 'contact',
