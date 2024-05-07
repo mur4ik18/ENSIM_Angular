18c201470c1671b37d390b58fe25d19d8b8e3905
Renaud44 <Segnon.Agbanglanon.Etu@univ-lemans.fr>
2024-05-07 15:30:34 +0100

    Les filtres avancées

---
+++ test/src/app/filtres-advanced/filtres-advanced.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtres-advanced',
+  standalone: true,
+  imports: [],
+  templateUrl: './filtres-advanced.component.html',
+  styleUrl: './filtres-advanced.component.css'
+})
+export class FiltresAdvancedComponent {
+
+}
