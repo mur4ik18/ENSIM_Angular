5a1a607232eebc18b032862ec22d75f229cdcded
Renaud44 <Segnon.Agbanglanon.Etu@univ-lemans.fr>
2024-05-23 18:34:19 +0100

    ui

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -5,12 +5,13 @@ import { DetailComponent } from './detail/detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule } from '@angular/router';
 import { ChoicePCComponent } from './choice-pc/choice-pc.component';
+import { FiltresAdvancedComponent } from './filtres-advanced/filtres-advanced.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent, ProductListComponent, DetailComponent, RouterModule,ChoicePCComponent],
+  imports: [HomeComponent, HeaderComponent, ProductListComponent, DetailComponent, RouterModule,ChoicePCComponent,FiltresAdvancedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
